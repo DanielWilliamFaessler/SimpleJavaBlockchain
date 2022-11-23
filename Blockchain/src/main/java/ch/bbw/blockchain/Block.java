4ea3685bf9ea385b4d434a08688ee87eae365a65
DanielWilliamFaessler <daniel.faesslerasmodi@gmail.com>
2022-11-23 14:27:41 +0100

    Added the final components to make transactions possible (added them into Block.java)

--- Blockchain/src/main/java/ch/bbw/blockchain/Block.java
+++ Blockchain/src/main/java/ch/bbw/blockchain/Block.java
@@ -1,6 +1,8 @@
 package ch.bbw.blockchain;
 
 import ch.bbw.blockchain.StringHashUtil;
+
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Block {
@@ -8,6 +10,8 @@ public class Block {
 	private long timeStamp; 
 	private String hash;
 	private String previousHash;
+	public String merkleRoot;
+	public ArrayList<Transaction> transactions = new ArrayList<Transaction>(); //our data will be a simple message.
 	private int nonce;
 	
 	public Block(String data, String previousHash) {
@@ -18,18 +22,38 @@ public class Block {
 	}
 
 	public String calculateHash() {
-		return StringHashUtil.getHashForString( data + Long.toString(timeStamp)+ Integer.toString(nonce) + previousHash );
+		return StringHashUtil.getHashForString(
+				data
+						+ Long.toString(timeStamp)
+						+ Integer.toString(nonce)
+						+ previousHash
+						+ merkleRoot);
 	}
 
 	public void mineBlock(int difficulty){
 
+		merkleRoot = StringHashUtil.getMerkleRoot(transactions);
 		String target = new String(new char[difficulty]).replace('\0','0');
 
 		while(!hash.substring(0,difficulty).equals(target)){
 			nonce ++;
 			hash = calculateHash();
 		}
-		System.out.println("Block Mined!! : " + hash);
+		System.out.println("Block Mined!! Thank the omnissiah: " + hash);
+	}
+
+	public boolean addTransaction(Transaction transaction) {
+		//process transaction and check if valid, unless block is genesis block then ignore.
+		if(transaction == null) return false;
+		if((previousHash != "0")) {
+			if((transaction.processTransaction() != true)) {
+				System.out.println("Transaction failed to process. Discarded.");
+				return false;
+			}
+		}
+		transactions.add(transaction);
+		System.out.println("Transaction Successfully added to Block");
+		return true;
 	}
 
 	public String getFullData() {
