56c2d97f7fc3841280984fb17ff5b0e628bdd08b
DanielWilliamFaessler <daniel.faesslerasmodi@gmail.com>
2022-11-23 14:07:15 +0100

    Added Test in Main to verify Signature

--- Blockchain/src/main/java/ch/bbw/blockchain/BlockChainTest.java
+++ Blockchain/src/main/java/ch/bbw/blockchain/BlockChainTest.java
@@ -1,13 +1,37 @@
 package ch.bbw.blockchain;
 
+import java.security.Security;
 import java.util.ArrayList;
 import com.google.gson.GsonBuilder;
 
 public class BlockChainTest {
 	public static int difficulty = 7;
 	public static ArrayList<Block> blockchain = new ArrayList<>();
+	public static Wallet walletA;
+	public static Wallet walletB;
 
 	public static void main(String[] args) {
+
+		//Setup Bouncey castle as a Security Provider
+		Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
+
+		//Create the new wallets
+		walletA = new Wallet();
+		walletB = new Wallet();
+
+		//Test public and private keys
+		System.out.println("Private and public keys:");
+		System.out.println(StringHashUtil.getStringFromKey(walletA.privateKey));
+		System.out.println(StringHashUtil.getStringFromKey(walletA.publicKey));
+
+		//Create a test transaction from WalletA to walletB
+		Transaction transaction = new Transaction(walletA.publicKey, walletB.publicKey, 5, null);
+		transaction.generateSignature(walletA.privateKey);
+
+		//Verify the signature works and verify it from the public key
+		System.out.println("The moment of truth is the signature verified");
+		System.out.println(transaction.verifiySignature());
+
 		//adding Blocks to Blockchain
 		System.out.println();
 		blockchain.add(new Block("Hi I am patient 0","0"));
